ba935aa4aa0a2ed2a16120100ee5c905dd83d2d1
fagustian <agustian.fuji@gmail.com>
2018-02-01 09:40:09 +0700

    ganti nama database

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,11 @@
 <?php 
-    $server='localhost';$user='root';$userPassword='12345';$db='webcourse';
-    $link = mysqli_connect($server, $user, $userPassword, $db)or die ("Waddaw ! Terjadi kesalahan..");
+    $server='localhost';$user='root';$userPassword='12345';$db='secretdiary';
+    $link = mysqli_connect($server, $user, $userPassword, $db);
 
-    // if (mysqli_connect_error()) {
-    //     die("Waddaw ! Terjadi kesalahan..");
-    // }
+    if (mysqli_connect_error()) {
+        die("Waddaw ! Terjadi kesalahan..");
+    }
+
+    // Cara lain pesan error
+    // $link = mysqli_connect($server, $user, $userPassword, $db))or die ("Waddaw ! Terjadi kesalahan..";
 ?>
